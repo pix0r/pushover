ee25260356eefdae699788fb33cde2a003724109
Mike Matz <mike@pixor.net>
2012-04-03 19:40:36 -0700

    Added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='pushover',
+        version='0.1',
+        url='https://github.com/pix0r/pushover',
+        py_modules=['pushover'],
+        scripts=['pushover.py'],
+        author='Mike Matz',
+        author_email='mike@pixor.net',
+        )
